11da1bece50af934744be923ad74b6118c3d18ce
Ian <reisian52@gmail.com>
2025-12-23 19:43:47 -0400

    style: christmas version and color theme

--- js/scripts.js
+++ js/scripts.js
@@ -4,54 +4,51 @@ const notChosen = [];
 
 // Preencher a tabela bingo com os 75 números
 const createTabel = () => {
-    const numberSheet = document.querySelector("#container-numbers");
-    numberSheet.innerHTML = "";
+  const numberSheet = document.querySelector("#container-numbers");
+  numberSheet.innerHTML = "";
 
-    for (let i = 1; i <= 75; i++) {
-        const cell = document.createElement("button");
-        cell.className = `number`;
-        cell.id = `${i}`;
-        cell.textContent = i;
-        notChosen.push(i);
+  for (let i = 1; i <= 75; i++) {
+    const cell = document.createElement("button");
+    cell.className = `number`;
+    cell.id = `${i}`;
+    cell.textContent = i;
+    notChosen.push(i);
 
-        numberSheet.appendChild(cell);
-    }
-}
+    numberSheet.appendChild(cell);
+  }
+};
 
 createTabel();
 
-
 // Atualizar último número
 const lastNumberUpdate = (number) => {
-    const lastNumber = document.querySelector(".last-number");
-    
-    if (number > 0 && number <= 15) {
-        lastNumber.innerText = "B" + number.toString().padStart(2, '0');
-    } else if (number <= 30) {
-        lastNumber.innerText = "I" + number;
-    } else if (number <= 45) {
-        lastNumber.innerText = "N" + number;
-    } else if (number <= 60) {
-        lastNumber.innerText = "G" + number;
-    } else {
-        lastNumber.innerText = "O" + number;
-    }
-}
+  const lastNumber = document.querySelector(".last-number");
+
+  if (number > 0 && number <= 15) {
+    lastNumber.innerText = "B" + number.toString().padStart(2, "0");
+  } else if (number <= 30) {
+    lastNumber.innerText = "I" + number;
+  } else if (number <= 45) {
+    lastNumber.innerText = "N" + number;
+  } else if (number <= 60) {
+    lastNumber.innerText = "G" + number;
+  } else {
+    lastNumber.innerText = "O" + number;
+  }
+};
 
 // Seleção de número
 const choseNumber = (number) => {
-
-    number.classList.add("chosen");
-    chosenNumbers.push(Number(number.id));
-    notChosen.splice(notChosen.indexOf(Number(number.id)), 1);
-    lastNumberUpdate(Number(number.id));
-
-}
+  number.classList.add("chosen");
+  chosenNumbers.push(Number(number.id));
+  notChosen.splice(notChosen.indexOf(Number(number.id)), 1);
+  lastNumberUpdate(Number(number.id));
+};
 
 // Remoção de número
 const removeNumber = (number) => {
-    // Gerar alerta
-    const alert = `<div class="alert">
+  // Gerar alerta
+  const alert = `<div class="alert">
         <p> Deseja remover o número ${number.id}?</p>
         <div class="container-options">
             <button class="option back"> Voltar </button>
@@ -59,51 +56,49 @@ const removeNumber = (number) => {
         </div>
     </div>`;
 
-    const parser = new DOMParser();
+  const parser = new DOMParser();
 
-    const htmlTemplate = parser.parseFromString(alert, "text/html");
+  const htmlTemplate = parser.parseFromString(alert, "text/html");
 
-    const alertBox = htmlTemplate.querySelector(".alert");
+  const alertBox = htmlTemplate.querySelector(".alert");
 
-    mainContainer.appendChild(alertBox);
+  mainContainer.appendChild(alertBox);
 
-    const backOption = document.querySelector(".back");
-    const removeOption = document.querySelector(".remove");
+  const backOption = document.querySelector(".back");
+  const removeOption = document.querySelector(".remove");
 
-    // Voltar
-    backOption.addEventListener('click', (e) => {
-        e.preventDefault();
-        alertBox.remove();
-    })
+  // Voltar
+  backOption.addEventListener("click", (e) => {
+    e.preventDefault();
+    alertBox.remove();
+  });
 
-    // Remoção confirmada
-    removeOption.addEventListener('click', (e) => {
-        e.preventDefault();
+  // Remoção confirmada
+  removeOption.addEventListener("click", (e) => {
+    e.preventDefault();
 
+    number.classList.remove("chosen");
 
-        number.classList.remove("chosen");
+    // Se número for o último escolhido
+    if (Number(number.id) == chosenNumbers[chosenNumbers.length - 1]) {
+      // Se houver um anterior
+      if (chosenNumbers[chosenNumbers.length - 2]) {
+        lastNumberUpdate(chosenNumbers[chosenNumbers.length - 2]);
+      } else {
+        const lastNumber = document.querySelector(".last-number");
+        lastNumber.innerText = "";
+      }
 
-        // Se número for o último escolhido
-        if (Number(number.id) == chosenNumbers[chosenNumbers.length - 1]) {
+      chosenNumbers.pop();
+    } else {
+      chosenNumbers.splice(chosenNumbers.indexOf(Number(number.id)), 1);
+    }
 
-            // Se houver um anterior
-            if (chosenNumbers[chosenNumbers.length - 2]) {
-                lastNumberUpdate(chosenNumbers[chosenNumbers.length - 2]);
-            } else {
-                const lastNumber = document.querySelector(".last-number");
-                lastNumber.innerText = "";
-            }
-
-            chosenNumbers.pop();
-        } else {
-            chosenNumbers.splice(chosenNumbers.indexOf(Number(number.id)), 1);
-        }
-        
-        notChosen.push(Number(number.id));
-        alertBox.remove();
-    })
-    return;
-}
+    notChosen.push(Number(number.id));
+    alertBox.remove();
+  });
+  return;
+};
 
 // Botões
 const bingoButton = document.querySelector("#bingo");
@@ -112,22 +107,27 @@ const newNumberButton = document.querySelector("#new-number");
 
 // Comemoração
 const celebrate = () => {
-    const celebrationImage = document.createElement("img");
-    celebrationImage.classList.add("celebration");
-    celebrationImage.src = "img/Bingo.webp";
-    celebrationImage.alt = "Imagem gerada por IA comemorativa de bingo";
+  const celebrationContainer = document.createElement("div");
+  celebrationContainer.classList.add("celebration-container");
 
-    mainContainer.appendChild(celebrationImage);
+  const celebrationImage = document.createElement("img");
+  celebrationImage.classList.add("celebration");
 
-    celebrationImage.addEventListener('click', (e) => {
-        e.preventDefault();
-        celebrationImage.remove();
-    })
+  celebrationImage.src = "img/Bingo.png";
+  celebrationImage.alt = "Imagem gerada por IA comemorativa de bingo";
+
+  celebrationContainer.appendChild(celebrationImage);
+  mainContainer.appendChild(celebrationContainer);
+
+  celebrationContainer.addEventListener("click", (e) => {
+    e.preventDefault();
+    celebrationContainer.remove();
+  });
 };
 
 const clear = () => {
-    // Gerar alerta
-    const alert = `<div class="alert">
+  // Gerar alerta
+  const alert = `<div class="alert">
     <p> Deseja remover TODOS OS NÚMEROS?</p>
     <div class="container-options">
         <button class="option back"> Voltar </button>
@@ -135,66 +135,65 @@ const clear = () => {
     </div>
     </div>`;
 
-    const parser = new DOMParser();
+  const parser = new DOMParser();
 
-    const htmlTemplate = parser.parseFromString(alert, "text/html");
+  const htmlTemplate = parser.parseFromString(alert, "text/html");
 
-    const alertBox = htmlTemplate.querySelector(".alert");
+  const alertBox = htmlTemplate.querySelector(".alert");
 
-    mainContainer.appendChild(alertBox);
+  mainContainer.appendChild(alertBox);
 
-    const backOption = document.querySelector(".back");
-    const removeOption = document.querySelector(".remove");
+  const backOption = document.querySelector(".back");
+  const removeOption = document.querySelector(".remove");
 
-    backOption.addEventListener('click', (e) => {
-        e.preventDefault();
-        alertBox.remove();
-    })
+  backOption.addEventListener("click", (e) => {
+    e.preventDefault();
+    alertBox.remove();
+  });
 
-    removeOption.addEventListener('click', () => {
-        location.reload();
-    })
+  removeOption.addEventListener("click", () => {
+    location.reload();
+  });
 };
 
 // Event listener do click nos números
 const numbers = document.querySelectorAll(".number");
 
-numbers.forEach(number => {
-    number.addEventListener('click', (e) => {
-        e.preventDefault();
-        if (number.classList.contains("chosen")) {
-            removeNumber(number);
-        } else {
-            choseNumber(number);
-        }
-    })
-})
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (number.classList.contains("chosen")) {
+      removeNumber(number);
+    } else {
+      choseNumber(number);
+    }
+  });
+});
 
 // Gerar novo número
 const generateNumber = () => {
-    if(notChosen.length == 0) {
-        celebrate();
-        return;
-    }
-
-    let newNumber = notChosen[Math.floor(Math.random() * notChosen.length)];
-    console.log(newNumber);
-
-    const numberEl = document.getElementById(`${newNumber}`)
-    choseNumber(numberEl);
-
-} 
-
-// Event listener dos botões 
-bingoButton.addEventListener('click', (e) => {
-    e.preventDefault();
+  if (notChosen.length == 0) {
     celebrate();
-})
-clearButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    clear();
-})
-newNumberButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    generateNumber();
-})
+    return;
+  }
+
+  let newNumber = notChosen[Math.floor(Math.random() * notChosen.length)];
+  console.log(newNumber);
+
+  const numberEl = document.getElementById(`${newNumber}`);
+  choseNumber(numberEl);
+};
+
+// Event listener dos botões
+bingoButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  celebrate();
+});
+clearButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  clear();
+});
+newNumberButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  generateNumber();
+});
